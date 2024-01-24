c2ddf4c72032435d4320e8aac06c010aa9e3b0e2
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 17:00:33 +0900

    ch02_variable 프로젝트

---
+++ Ch02_Variable/src/ch02/operator02/ArithOp.java
@@ -0,0 +1,15 @@
+package ch02.operator02;
+
+public class ArithOp {
+	public static void main(String[] args) {
+		// 사칙연산
+		int num0 = 10;
+		int num1 = 3;
+		System.out.println(num0+num1);
+		System.out.println(num0-num1);
+		System.out.println(num0*num1);
+		System.out.println(num0/num1);	//몫
+		System.out.println(num0%num1);	//나머지
+	}
+
+}
