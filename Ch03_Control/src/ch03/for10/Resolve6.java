70230058999af9a1ada61e76fc2a15c480cee1e3
NCP08 <navercloudplatform08.gmail.com>
2024-01-29 09:41:05 +0900

    ch03.for10.Resolve6 수정

--- Ch03_Control/src/ch03/for10/Resolve6.java
+++ Ch03_Control/src/ch03/for10/Resolve6.java
@@ -1,15 +1,18 @@
 package ch03.for10;
 
+//6. do~while문을 이용해서 1부터 10까지 합을 출력하세요
 public class Resolve6 {
 	public static void main(String[] args) {
-//		6. do~while문을 이용해서 1부터 10까지 합을 출력하세요
-		int i = 1, sum = 1; 
+		int i=1,sum=0;
 		do {
-			System.out.println(i + ", " + sum);
-			i++;
 			sum = sum + i;
+			System.out.println("누적 : " + sum);
+			i++;
+			
 			
 		}while(i<=10);
+		System.out.println("1~10의 총합 : "+sum);
 	}
+	// 수정
 
 }
