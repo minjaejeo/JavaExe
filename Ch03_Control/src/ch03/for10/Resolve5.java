33ddfc25ebbd905c7138eb0c0f693c8917a41707
NCP08 <navercloudplatform08.gmail.com>
2024-01-29 09:27:37 +0900

    ch03.for10.Resolve5 수정

--- Ch03_Control/src/ch03/for10/Resolve5.java
+++ Ch03_Control/src/ch03/for10/Resolve5.java
@@ -1,13 +1,14 @@
 package ch03.for10;
 
+//5. do~while문을 이용해서 10부터 20까지 출력하세요
 public class Resolve5 {
 	public static void main(String[] args) {
-//		5. do~while문을 이용해서 10부터 20까지 출력하세요
 		int i = 10;
 		do {
 			System.out.println(i);
 			i++;
 		}while(i<=20);
 	}
+	// 수정했다.
 
 }
