1b2315e5926e80e9dc9d387c6c8ba3a4a1794984
minajeajeo <navercloudplatform.gmail.com>
2024-02-13 23:22:42 +0900

    2.13 colletion01.ch15 복습다했다 + 과제

--- Ch10_Collection/src/ch10/collection01/ch15/sec08/ImmutableExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec08/ImmutableExample.java
@@ -1,6 +1,11 @@
 package ch10.collection01.ch15.sec08;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class ImmutableExample {
@@ -13,7 +18,38 @@ public class ImmutableExample {
 		Set<String> immutableSet = Set.of("A","B","C");
 		//immutableSet.remove("A"); (x)
 		
+		//Map 불변 컬렉션 생성
+		Map<Integer, String> immutableMap1 = Map.of(
+				1, "A",
+				2, "B",
+				3, "C"
+				);
+		//immutableMap1.put(4, "D"); (x)
 		
+		//List 컬렉션을 불변 컬렉션으로 복사
+		List<String> list = new ArrayList< >();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		List<String> immutableList2 = List.copyOf(list);
+		
+		//Set 컬렉션을 불변 컬렉션으로 복사
+		Set<String> set = new HashSet< >();
+		set.add("A");
+		set.add("B");
+		set.add("C");
+		Set<String> immutableSet2 = Set.copyOf(set);
+		
+		//Map 컬렉션을 불변 컬렉션으로 복사
+		Map<Integer, String> map = new HashMap< >();
+		map.put(1, "A");
+		map.put(2, "B");
+		map.put(3, "C");
+		Map<Integer, String> immutableMap2 =  Map.copyOf(map);
+		
+		//배열로부터 List 불변 컬렉션 생성
+		String[] arr = {"A","B","C"};
+		List<String> immutableList3 = Arrays.asList(arr);
 	}
 
 }
