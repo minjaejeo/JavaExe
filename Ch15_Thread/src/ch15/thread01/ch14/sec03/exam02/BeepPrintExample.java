c0fb1f2ccc501016794016e243830745930578f0
minajeajeo <navercloudplatform.gmail.com>
2024-02-15 23:30:17 +0900

    Thread01.ch14.sec03까지 집에서 복습 2.15

--- Ch15_Thread/src/ch15/thread01/ch14/sec03/exam02/BeepPrintExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec03/exam02/BeepPrintExample.java
@@ -5,25 +5,25 @@ import java.awt.Toolkit;
 public class BeepPrintExample {
 	public static void main(String[] args) {
 		//작업 스레드 생성
-		//Worker 스레드 생성 후 -> start () -> run() 실행
+		// Worker 스레드 생성 후 -> start() -> run() 실행
 		Thread thread = new Thread(new Runnable() {
+			// 작업 스레드 실행
 			@Override
-			// 작업스레드 실행
 			public void run() {
 				Toolkit toolkit = Toolkit.getDefaultToolkit();
 				for(int i=0;i<5;i++) {
 					toolkit.beep();
-					try {Thread.sleep(500);} catch(Exception e) {}
+					try {Thread.sleep(500);}catch(Exception e) {}
 				}
 			}
 		});
+		
 		thread.start();
 		
 		for(int i=0;i<5;i++) {
 			System.out.println("띵");
-			try {Thread.sleep(500);} catch(Exception e) {}
+			try {Thread.sleep(500);}catch(Exception e) {}
 		}
-		
 	}
 
 }
--- Ch15_Thread/src/ch15/thread01/ch14/sec03/exam03/BeepPrintExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec03/exam03/BeepPrintExample.java
@@ -3,27 +3,34 @@ package ch15.thread01.ch14.sec03.exam03;
 import java.awt.Toolkit;
 
 public class BeepPrintExample {
-	//main 함수는 main 스레드가 해야할 일
+	// main 함수는 main 스레드가 해야할 일
 	public static void main(String[] args) {
-		//main스레드(주인아저씨)가 worker스레드(알바생)을 고용한다.
+		// main스레드(주인아저씨)가 worker스레드(알바생)을 고용한다.
 		Thread thread = new Thread() {
-			//알바생이 해야할 일
+			// 알바생이 해야할 일
 			@Override
 			// 작업 스레드 실행
 			public void run() {
 				Toolkit toolkit = Toolkit.getDefaultToolkit();
-				for(int i=0;i<5;i++) {
+				for (int i = 0; i < 5; i++) {
 					toolkit.beep();
-					try {Thread.sleep(500);} catch(Exception e) {}
+					try {
+						Thread.sleep(500);
+					} catch (Exception e) {
+					}
 				}
 			}
 		};
-		//worker스레드 (알바생) 일 시작해
+		// worker스레드(알바생) 일 시작해
 		thread.start();
-		for(int i=0;i<5;i++) {
+
+		for (int i = 0; i < 5; i++) {
 			System.out.println("띵");
-			try {Thread.sleep(500);} catch(Exception e ) {}
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+			}
 		}
 	}
-
 }
+
