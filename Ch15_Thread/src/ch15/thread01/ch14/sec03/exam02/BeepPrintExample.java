f4e3d954e6fdef477b9269ecf83afbc7a2764d48
NCP08 <navercloudplatform08.gmail.com>
2024-02-28 17:49:29 +0900

    thread01.ch14.exam03하는중

--- Ch15_Thread/src/ch15/thread01/ch14/sec03/exam02/BeepPrintExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec03/exam02/BeepPrintExample.java
@@ -4,26 +4,32 @@ import java.awt.Toolkit;
 
 public class BeepPrintExample {
 	public static void main(String[] args) {
-		//작업 스레드 생성
 		// Worker 스레드 생성 후 -> start() -> run() 실행
 		Thread thread = new Thread(new Runnable() {
-			// 작업 스레드 실행
 			@Override
 			public void run() {
 				Toolkit toolkit = Toolkit.getDefaultToolkit();
 				for(int i=0;i<5;i++) {
-					toolkit.beep();
-					try {Thread.sleep(500);}catch(Exception e) {}
+					toolkit.beep();// 비프음 발생
+					try {
+						Thread.sleep(500);//0.5초간 일시 정지
+					}catch(Exception e) {
+						
+					}
+					
 				}
+				
 			}
 		});
-		
 		thread.start();
 		
 		for(int i=0;i<5;i++) {
 			System.out.println("띵");
-			try {Thread.sleep(500);}catch(Exception e) {}
+			try {
+				Thread.sleep(500);//0.5초간 일시 정지
+			}catch(Exception e) {
+				
+			}
 		}
 	}
-
 }
--- Ch15_Thread/src/ch15/thread01/ch14/sec03/exam03/BeepPrintExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec03/exam03/BeepPrintExample.java
@@ -5,30 +5,31 @@ import java.awt.Toolkit;
 public class BeepPrintExample {
 	// main 함수는 main 스레드가 해야할 일
 	public static void main(String[] args) {
-		// main스레드(주인아저씨)가 worker스레드(알바생)을 고용한다.
-		Thread thread = new Thread() {
-			// 알바생이 해야할 일
+		// Worker 스레드 생성 후 -> start() -> run() 실행
+		Thread thread = new Thread(new Runnable() {
 			@Override
-			// 작업 스레드 실행
 			public void run() {
 				Toolkit toolkit = Toolkit.getDefaultToolkit();
-				for (int i = 0; i < 5; i++) {
-					toolkit.beep();
+				for(int i=0;i<5;i++) {
+					toolkit.beep();// 비프음 발생
 					try {
-						Thread.sleep(500);
-					} catch (Exception e) {
+						Thread.sleep(500);//0.5초간 일시 정지
+					}catch(Exception e) {
+						
 					}
+					
 				}
+				
 			}
-		};
-		// worker스레드(알바생) 일 시작해
+		});
 		thread.start();
-
-		for (int i = 0; i < 5; i++) {
+		
+		for(int i=0;i<5;i++) {
 			System.out.println("띵");
 			try {
-				Thread.sleep(500);
-			} catch (Exception e) {
+				Thread.sleep(500);//0.5초간 일시 정지
+			}catch(Exception e) {
+				
 			}
 		}
 	}
