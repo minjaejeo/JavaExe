df352648f48d9d4ffbe8ce4135520d3bcafcc84d
NCP08 <navercloudplatform08.gmail.com>
2024-02-28 17:58:33 +0900

    thread01.ch14.sec03 완료

--- Ch15_Thread/src/ch15/thread01/ch14/sec03/exam03/BeepPrintExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec03/exam03/BeepPrintExample.java
@@ -5,8 +5,9 @@ import java.awt.Toolkit;
 public class BeepPrintExample {
 	// main 함수는 main 스레드가 해야할 일
 	public static void main(String[] args) {
-		// Worker 스레드 생성 후 -> start() -> run() 실행
-		Thread thread = new Thread(new Runnable() {
+		// main스레드(주인아저씨)가 work스레드(알바생)을 고용한다.
+		Thread thread = new Thread() {
+			// 알바생이 해야할 일
 			@Override
 			public void run() {
 				Toolkit toolkit = Toolkit.getDefaultToolkit();
@@ -21,7 +22,8 @@ public class BeepPrintExample {
 				}
 				
 			}
-		});
+		};
+		// worker 스레드(알바생) 일 시작해
 		thread.start();
 		
 		for(int i=0;i<5;i++) {
