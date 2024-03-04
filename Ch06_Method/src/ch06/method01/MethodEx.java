ad71c48a3b2a344a068e98a4a1490cb47a56d37f
NCP08 <navercloudplatform08.gmail.com>
2024-03-04 17:35:26 +0900

    Method복습하자

--- Ch06_Method/src/ch06/method01/MethodEx.java
+++ Ch06_Method/src/ch06/method01/MethodEx.java
@@ -7,16 +7,16 @@ public class MethodEx {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		// 1) 처리하기 전에 입력받는 부분
-		System.out.print("1번째 정수 입력 >>	");
+		//1) 처리하기 전에 입력받는 부분
+		System.out.print("1번째 정수 입력 >> ");
 		int num0 = sc.nextInt();
 		System.out.print("2번째 정수 입력 >> ");
 		int num1 = sc.nextInt();
-		System.out.print("연산자 입력(+ - * / %) >> ");
+		System.out.println("연산자 입력(+ - * / %) >> ");
 		String op = sc.next();
-		int result = 0;		// 결과값 저장 변수
+		int result = 0;	//결과값 저장 변수
 		
-		// 2) 계산하는 부분
+		// 2) 계산을 하는 부분
 		switch(op) {
 		case "+":
 			result = num0 + num1;
@@ -33,17 +33,12 @@ public class MethodEx {
 		case "%":
 			result = num0 % num1;
 			break;
-		default:
-			System.out.println("연산이 불가능합니다.");
-			break;
+			default:
+				System.out.println("연산이 불가능합니다.");
 		}
-		
-		// 3) 결과값 출력
-		System.out.println("결과값은 " + result + " 입니다.");
+		//3) 결과값 출력
+		System.out.println("결과값은 " +result + "입니다.");
 		
 		sc.close();
-		
-		
 	}
-	
 }
